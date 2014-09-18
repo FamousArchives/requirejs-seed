d16047c72d2c98bf60ff9c0225fe66b3d6e34771
Myles Borins <myles@famo.us>
2014-09-18 01:56:57 -0700

    Generated from latest generator

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,6 +1,11 @@
+
+
+
 module.exports = function (grunt) {
   'use strict';
   grunt.registerTask('serve', function (target) {
+    
+
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
